6d5b82a32fe2f41ed9822ab900768338911e5484
KozachukAP <aleksandr.kozachuk@stm-labs.ru>
2024-08-06 09:52:38 +0300

    task-2: add sharding case

---
+++ mongo-sharding/start_cluster.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+echo "Hello, World!"
+
+configSrvRS="config-server-rs"
+configSrv="configSrv"
+
+shard1Rs="shard1-rs"
+shard1="shard1"
+shard2Rs="shard2-rs"
+shard2="shard2"
+
+dbname="somedb"
+docname="helloDoc"
+
+defaultPort=27017
+
+echo "===Run cluster=============================================="
+docker-compose up -d
+
+echo "===Init config Server======================================="
+docker-compose exec -T configSrv mongosh --port 27017 <<EOF
+rs.initiate(
+  {
+    _id: "config-server-rs",
+    configsvr: true,
+    members: [
+      { _id : 0, host : "configSrv:27017" },
+    ]
+  }
+)
+EOF
+echo "============================================================"
+
+echo "===Wait 5s================================================="
+sleep 5
+
+echo "===Init shard 1 ============================================"
+docker compose exec -T shard1 mongosh --port 27017 <<EOF
+rs.initiate( { _id : "shard1-rs", members: [{ _id : 0, host : "shard1:27017" }, ] } );
+EOF
+
+echo "===Init shard 2 ============================================"
+docker compose exec -T shard2 mongosh --port 27017 <<EOF
+rs.initiate(
+    {
+      _id : "shard2-rs",
+      members: [
+        { _id : 0, host : "shard2:27017" },
+      ]
+    }
+);
+EOF
+
+echo "===Init router ============================================"
+
+docker-compose exec -T router mongosh --port 27017 <<EOF
+sh.addShard( "shard1-rs/shard1:27017");
+sh.addShard( "shard2-rs/shard2:27017");
+sh.enableSharding("somedb");
+sh.shardCollection("somedb.helloDoc", { "name" : "hashed" } );
+EOF
+
+echo "===Configuration is done==================================="
+
+echo "===Fill data==============================================="
+
+docker-compose exec -T router mongosh <<EOF
+use ${dbname}
+for(var i = 0; i < 1000; i++) db.${docname}.insertOne({age:i, name:"ly"+i})
+EOF
+
+echo "===Check count of data via router=========================="
+
+docker compose exec -T router mongosh --port ${defaultPort} <<EOF
+use ${dbname};
+db.${docname}.countDocuments();
+EOF
+
+echo "===Check count of data via shard1========================="
+
+docker compose exec -T ${shard1} mongosh --port ${defaultPort} <<EOF
+use ${dbname};
+db.${docname}.countDocuments();
+EOF
+
+echo "===Check count of data via shard1========================="
+
+docker compose exec -T ${shard2} mongosh --port ${defaultPort} <<EOF
+use ${dbname};
+db.${docname}.countDocuments();
+EOF
+
+echo "===Check via rest api==================================="
+RESPONSE=$(curl http://localhost:8080/$docname/count)
+countOfDocuments=`echo $RESPONSE | awk -F"," '{ print $3 }'  | awk -F":" '{ print substr($2, 1, length($2)-1) }'`
+
+if [[ $countOfDocuments != 0 ]]; then
+  echo "======================================================"
+  echo "Success. Current number of documents: ${countOfDocuments} != 0";
+else
+  echo "Failed. Current number of documents: ${countOfDocuments}";
+fi
+echo "======================================================"
